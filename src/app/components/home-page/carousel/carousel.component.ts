200d7e716e580bfa7114fec9799e7f18fc103466
Pablo Araújo <rafanet.live@gmail.com>
2020-05-18 21:56:42 -0300

    Finalizando Caption.

--- src/app/components/home-page/carousel/carousel.component.ts
+++ src/app/components/home-page/carousel/carousel.component.ts
@@ -2,42 +2,62 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
-  styles: [''],
+  styleUrls: ['./carousel.component.scss'],
   template: `
     <carousel>
     <slide>
       <img src="../../../../assets/images/carousel/1.jpg" alt="first slide" style="display: block; width: 100%;">
-      <div class="carousel-caption d-none d-md-block">
-        <h3>First slide label</h3>
+      <div class="carousel-caption">
+        <h3>As melhores tendências do mundo da moda.</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-caption__btn">
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+        </div>
       </div>
     </slide>
     <slide>
       <img src="../../../../assets/images/carousel/2.jpg" alt="second slide" style="display: block; width: 100%;">
-      <div class="carousel-caption d-none d-md-block">
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div class="carousel-caption">
+        <h3>As melhores tendências do mundo da moda.</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-caption__btn">
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+        </div>
       </div>
     </slide>
     <slide>
       <img src="../../../../assets/images/carousel/3.jpg" alt="third slide" style="display: block; width: 100%;">
-      <div class="carousel-caption d-none d-md-block">
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <div class="carousel-caption">
+        <h3>As melhores tendências do mundo da moda.</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-caption__btn">
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+        </div>
       </div>
     </slide>
     <slide>
       <img src="../../../../assets/images/carousel/4.jpg" alt="third slide" style="display: block; width: 100%;">
-      <div class="carousel-caption d-none d-md-block">
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <div class="carousel-caption">
+        <h3>As melhores tendências do mundo da moda.</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-caption__btn">
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+        </div>
       </div>
     </slide>
     <slide>
       <img src="../../../../assets/images/carousel/5.jpg" alt="third slide" style="display: block; width: 100%;">
-      <div class="carousel-caption d-none d-md-block">
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <div class="carousel-caption">
+        <h3>As melhores tendências do mundo da moda.</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-caption__btn">
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+          <button class="btn btn-default-loja btn-lg">Conferir</button>
+        </div>
       </div>
     </slide>
   </carousel>
